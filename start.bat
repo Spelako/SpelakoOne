510f7ce3f3b1ce136c6407a00d3676c4fa0d94a1
Peaksol <1244050218@qq.com>
2022-02-14 01:10:05 +0800

    launcher update

--- start.bat
+++ start.bat
@@ -2,4 +2,5 @@
 title SpelakoOne
 :start
 php SpelakoOne.php --core="../SpelakoCore/SpelakoCore.php" --config="config.json" --host="http://127.0.0.1:5700"
+timeout /t 10 /nobreak
 goto start
--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 while :
 do
 php SpelakoOne.php --core="../SpelakoCore/SpelakoCore.php" --config="config.json" --host="http://127.0.0.1:5700"
+echo "Waiting for 10 seconds, press Ctrl+C to quit ..."; sleep 10
 done
